edb5d0ee65f2e81d16e9db20f0326a20dcf5fb55
funfun-ai <business@funfun.ai>
2024-07-14 09:56:21 +0000

    WUP

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
+        </p> */}
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -38,6 +38,13 @@ export default function Home() {
           priority
         />
       </div>
+
+      <h1 className="text-4xl text-center">
+        Discorver Our Top AI Tools
+      </h1>
+      <h2 className="text-sm text-center max-w-lg">
+        Explore a comprehensive collection of AI tools tailored for every need. Whether you’re a developer, researcher, or enthusiast, our directory offers the latest and greatest in AI technology, from machine learning to image generation and beyond. Find the perfect tools to boost your productivity and creativity all in one place.
+      </h2>
 
     <ul className="space-y-8 text-center">
       <li>
