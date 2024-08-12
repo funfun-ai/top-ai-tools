a6e81b6103addcdfcc3d72acb1a179140559c950
funfun-ai <business@funfun.ai>
2024-08-12 21:18:58 +0000

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -83,6 +83,15 @@ export default function Home() {
           Best AI Chatbot
           </a>
       </li>
+      <li>
+      <a
+          href="https://www.funfun.ai/tools/ai-girl-generator"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+        >
+          Best AI Girl Generator
+          </a>
+      </li>
     </ul>
       
 
